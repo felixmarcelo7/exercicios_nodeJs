aaf13892583b0a802613f2f8035182838e5cfaa1
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-10-02 14:25:26 -0300

    aula 66 - 4_NPM_FUNDAMENTAL

---
+++ 4_NPM_FUNDAMENTAL/4_dependencia_dev/index.js
@@ -0,0 +1,21 @@
+//há uma possibilidade de instalar pacotes apenas para o ambiente de desenvolvimento
+//Utilizamos a flag --save-dev
+//isso faz com que ele seja separado no package.json dos demais
+//então na build de produção não instalaremos estes módulos
+//um exemplo: servidor para ambientelocal como o Nodemon;
+
+const _ = require('lodash');
+const chalk = require('chalk');
+
+const a = [1, 2, 3, 4, 5, 6];
+const b = [5, 6, 7, 8, 9];
+
+const diff = _.difference(a, b);
+
+console.log(chalk.bgRed.bold(diff));
+
+//Constantemente os pacotes do npm são atualizados
+//temos então ocomando npm update
+//que vi fazer a atulização de todos os pacotes instalados no package.json
+//é possível atualizar um pacote específico com 'npm update <nome>'
+//para checar se existe novas versões se o npm update n funcionar 'npx npm-check-updates -u'

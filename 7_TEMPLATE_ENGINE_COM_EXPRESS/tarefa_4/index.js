228298887c9aa8ca38796653e9fd885a7e59812e
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-11-14 17:34:21 -0300

    tarefa 4 - feita - 7_TEMPLATE_ENGINE_COM_EXPRESS

---
+++ 7_TEMPLATE_ENGINE_COM_EXPRESS/tarefa_4/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+const products = [
+  {
+    id: "1",
+    title: "Livro",
+    price: 20.99,
+  },
+  {
+    id: "2",
+    title: "Caderno",
+    price: 15.99,
+  },
+  {
+    id: "3",
+    title: "Mochila",
+    price: 200.99,
+  },
+];
+
+app.get("/product/:id", (req, res) => {
+  const product = products[parseInt(req.params.id) - 1];
+
+  res.render("product", { product });
+});
+
+app.get("/", (req, res) => {
+  res.render("home", { products });
+});
+
+app.listen(3000, () => {
+  console.log("App rodadno");
+});

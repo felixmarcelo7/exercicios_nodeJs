93b2460a158efa58a1873865c99b928579e99cb8
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2024-11-19 23:09:14 -0300

    feature de adicionar um produto - pronta

--- 7_TEMPLATE_ENGINE_COM_EXPRESS/tarefa_4/index.js
+++ 7_TEMPLATE_ENGINE_COM_EXPRESS/tarefa_4/index.js
@@ -4,23 +4,24 @@ const exphbs = require("express-handlebars");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 const products = [
   {
-    id: "1",
+    id: 1,
     title: "Livro",
     price: 20.99,
   },
   {
-    id: "2",
+    id: 2,
     title: "Caderno",
     price: 15.99,
   },
   {
-    id: "3",
+    id: 3,
     title: "Mochila",
     price: 200.99,
   },
@@ -30,6 +31,13 @@ app.get("/product/:id", (req, res) => {
   const product = products[parseInt(req.params.id) - 1];
 
   res.render("product", { product });
+});
+
+app.post("/", (req, res) => {
+  req.body.id = products.length + 1;
+  products.push(req.body);
+
+  res.render("home", { products });
 });
 
 app.get("/", (req, res) => {
--- 7_TEMPLATE_ENGINE_COM_EXPRESS/tarefa_4/public/scripts/object.js
+++ 7_TEMPLATE_ENGINE_COM_EXPRESS/tarefa_4/public/scripts/object.js
@@ -1,13 +1,24 @@
 const btn = document.querySelector("button");
 
-
-
-
-
 btn.addEventListener("click", () => {
-    const title = prompt("Digite o nome do produto: ");
-    const price = prompt("Digite o preço do produto: ");
-    const id = products.length + 1;
+  const title = prompt("Digite o nome do produto: ");
+  const price = Number(prompt("Digite o preço do produto: "));
 
-    products.push({id, title, price});
+  const newProduct = { title, price };
+
+  if (title === null || isNaN(price) || price === 0) {
+    return;
+  } else {
+    fetch("http://localhost:3000", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    }).catch((error) => {
+      console.error("Erro ao enviar os dados:", error);
+    });
+
+    window.location.reload();
+  }
 });

3410bf0f985f46668deb313fb84e905d9016d1f6
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2025-02-19 15:35:47 -0300

    aula 125 - 8_INTEGRACAO_NODE_MSQL

--- 8_INTEGRACAO_NODE_MSQL/2_inserindo_dados/index.js
+++ 8_INTEGRACAO_NODE_MSQL/2_inserindo_dados/index.js
@@ -4,14 +4,39 @@ const mysql = require("mysql");
 
 const app = express();
 
+//pega o body em .json
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json())
+
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+//referencia a pasta public para renderizar os estilos
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+//faz o poste dos dados do corpo da requisição
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ("${title}","${pageqty}")`;
+
+  //conecta a query e verifica erro
+  conn.query(sql, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+})
 
 const conn = mysql.createConnection({
   host: "localhost",

0ac4d871efb3872887dab04d8e99d3ff05e1c519
felixmarcelo7 <felixmarcelo.dev@gmail.com>
2025-02-20 14:54:29 -0300

    aula 126 - 8_INTEGRACAO_NODE_MSQL

--- 8_INTEGRACAO_NODE_MSQL/3_resgatando-dados/index.js
+++ 8_INTEGRACAO_NODE_MSQL/3_resgatando-dados/index.js
@@ -7,10 +7,10 @@ const app = express();
 //pega o body em .json
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
 );
-app.use(express.json())
+app.use(express.json());
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-//faz o post dos dados do corpo da requisição
+//faz o poste dos dados do corpo da requisição
 app.post("/books/insertbook", (req, res) => {
   const title = req.body.title;
   const pageqty = req.body.pageqty;
@@ -30,13 +30,32 @@ app.post("/books/insertbook", (req, res) => {
   const sql = `INSERT INTO books (title, pageqty) VALUES ("${title}","${pageqty}")`;
 
   //conecta a query e verifica erro
-  conn.query(sql, function(err) {
-    if(err) {
+  conn.query(sql, function (err) {
+    if (err) {
       console.log(err);
+      return;
     }
     res.redirect("/");
   });
-})
+});
+
+//recupera os dados do banco
+app.get("/books", (req, res) => {
+  const sql = "SELECT * FROM books";
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const books = data;
+
+    console.log(books);
+
+    res.render("books", { books });
+  });
+});
 
 const conn = mysql.createConnection({
   host: "localhost",
